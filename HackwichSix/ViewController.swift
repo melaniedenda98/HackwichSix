59bd151345ac2136bce477a84e8dda95b6d280f7
melaniedenda98 <mdenda@hawaii.edu>
2022-10-27 05:51:29 -0700

    Updated Repository

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -7,7 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    var myFriendsArray = ["Mel", "Will","Kim"]
+    var myCountriesArray = ["Japan", "China", "South Korea", "Indonesia", "North America"]
+    var friendsHomeArray = ["Mililani", "Gresham", "Aiea"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriendsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = myFriendsArray[indexPath.row]
+        cell?.textLabel?.text = text
+        cell?.detailTextLabel?.text = friendsHomeArray[indexPath.row]
+        return cell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
